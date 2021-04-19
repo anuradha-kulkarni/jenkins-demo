d01c69c158a00c62e90ddcc083803c885a214dcd
code-with-madhu <73005757+code-with-madhu@users.noreply.github.com>
2021-04-19 06:24:29 +0000

    Update requests_program.py

--- requests_program.py
+++ requests_program.py
@@ -2,5 +2,6 @@
 import requests
 ssl = 'authenticate.cert'
 resp = requests.get('http://httpbin.org/ip')
+print("*"*30)
 print("IP address of Jenkins host is ------------------{}-----------------".format(resp.text))
 print("*"*30)

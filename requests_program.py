45e0ced4fc07b8b6563f84f4a1198d2b7268acd2
code-with-madhu <73005757+code-with-madhu@users.noreply.github.com>
2021-05-12 18:34:07 +0530

    Update requests_program.py

--- requests_program.py
+++ requests_program.py
@@ -1,6 +1,6 @@
 
 import requests
 resp = requests.get('http://httpbin.org/ip')
-print("*"*30)
+print("*"*50)
 print("IP address of Jenkins host is ------------------{}-----------------".format(resp.text))
-print("*"*30)
+print("*"*50)
